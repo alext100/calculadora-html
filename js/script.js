f77be328f697ea7cc880efad4170b0fdb93b5c53
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-09-03 17:45:21 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const keys = calculator.querySelector('.calculator__keys');
 let n1 = "0";
 let number1 = "";
 let number2 = "";
-let operator = 0;
+let operator = "";
 let previousKeyType;
-let result = 0;
+let result = "";
 
 const calculate = (n1, operator, n2) => {
     const firstNum = parseFloat(n1);
@@ -25,10 +25,50 @@ calculator.addEventListener("click", function typeOfKey(event) {
             case "number":
                 if (display.textContent === "") {
                     n1 = event.target.textContent;
+                    display.textContent = n1;
                 } else if (display.textContent !== "") {
                     n1 += event.target.textContent;
+                    display.textContent = n1;
                 }
-                display.textContent += n1;
+                //display.textContent += n1;
+                break;
+            case "add":
+            case "subtract":
+            case "divide":
+            case "multiply":
+                if (display.textContent === "") {
+                    break;
+                } else if (result !== "" && (display.textContent.includes("+") || display.textContent.includes("-") || display.textContent.includes("÷") || display.textContent.includes("×"))) {
+                    number1 = n1;
+                    operator = event.target.className;
+                    display.textContent = number1 + event.target.textContent;
+                }
+                /* else if (result !== "") {
+                                   // number1 = display.textContent;
+                                   number1 = result;
+                                   operator = event.target.className;
+                                   n1 = "";
+                                   display.textContent += event.target.textContent;
+                               }  */
+                else if (display.textContent.includes("+") || display.textContent.includes("-") || display.textContent.includes("÷") || display.textContent.includes("×")) {
+                    console.log(`attencion ${display.textContent}`);
+                    number2 = n1;
+
+                    result = calculate(number1, operator, number2);
+                    operator = event.target.className;
+                    display.textContent = result + event.target.textContent;
+                    number1 = result;
+                    number2 = "";
+                    n1 = "";
+                    console.log(`number1 =  ${number1}, number2 =  ${number2}`);
+                    break;
+                } else if (result === "") {
+                    number1 = n1;
+                    n1 = "";
+                    operator = event.target.className;
+                    display.textContent += event.target.textContent;
+                    break;
+                }
                 break;
             case "comma":
                 if (display.textContent === "0") {
@@ -42,41 +82,18 @@ calculator.addEventListener("click", function typeOfKey(event) {
                 display.textContent = n1;
                 break;
             case "ac":
-                display.textContent = 0;
+                display.textContent = "";
                 n1 = "";
                 number1 = "";
                 number2 = "";
                 result = "";
-                operator = 0;
+                operator = "";
                 break;
             case "del":
-                if (display.textContent !== "0") {
+                if (display.textContent !== "") {
                     n1 = display.textContent.split("").slice(0, -1).join('');
                 }
                 display.textContent = n1;
-                break;
-            case "add":
-            case "subtract":
-            case "divide":
-            case "multiply":
-                if (display.textContent !== "" && (display.textContent.includes("+") || display.textContent.includes("-") || display.textContent.includes("÷") || display.textContent.includes("×"))) {
-                    console.log(`attencion ${display.textContent}`);
-                    number2 = n1;
-                    result = calculate(number1, operator, number2);
-                    display.textContent = result + event.target.textContent;
-                    number1 = result;
-                    break;
-                } else if (result !== 0) {
-                    number1 = display.textContent;
-                    operator = event.target.className;
-                    n1 = "";
-                    display.textContent += event.target.textContent;
-                } else if (result === 0) {
-                    number1 = n1;
-                    n1 = "";
-                    operator = event.target.className;
-                    display.textContent += event.target.textContent;
-                }
                 break;
             case "enter":
                 number2 = n1;
